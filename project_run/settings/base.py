8d461b067f4613acfe487cfe9f17b91ddca73a7a
AlexandrBlackJ <aseevalexandr@bk.ru>
2026-08-16 17:06:37 +0300

    add home page

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Спринтеры России'
+SLOGAN = 'Движение это Жизнь'
+CONTACTS = 'beg_rf@beg.ru'
