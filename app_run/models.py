87462233d3602d56410ef0f7ce9b4e6cdc295aed
AlexandrBlackJ <aseevalexandr@bk.ru>
2026-08-17 09:52:51 +0300

    add models

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Runer(models.Model):
+    username = models.CharField(primary_key=True, max_length=50, db_index=True, null=False)
+
+
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    athlete = models.ForeignKey(Runer, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=100)
